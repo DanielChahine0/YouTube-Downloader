0a66d59a2d49942dbcf5d6661cb50f6bf4ec46b7
DanielChahine0 <Chahinedaniel0@gmail.com>
2024-09-13 12:51:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,69 @@
 import tkinter
-import customtkinter # type: ignore
-from pytube import YouTube # type: ignore
+import customtkinter
+from pytubefix import YouTube
+import os
 
 
 def startDownload():
-    ytLink = link.get()
-    ytObject = YouTube(ytLink)
-    # video = ytObject.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-    video = ytObject.streams.get_highest_resolution()
-    PATH = r"C:\Users\chahi\Downloads"
-    video.download(PATH,'videoFilename','mp4')
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+        video = ytObject.streams.get_highest_resolution()
+        title.configure(text=ytObject.title, text_color="white")
+        finishLabel.configure(text="")
+        video.download(output_path=os.path.join(os.path.expanduser('~'), 'Downloads'))
+        finishLabel.configure(text="Downloaded!", text_color="green")
 
-# System settings
+    except Exception as e:
+        finishLabel.configure(text="Download Error", text_color="red")
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_dowloaded = total_size - bytes_remaining
+    percentage_of_completion = (bytes_dowloaded / total_size) * 100
+    per = str(int(percentage_of_completion))
+    pPercentage.configure(text=per + '%')
+    pPercentage.update()
+
+    # Update Progress Bar
+    progressBar.set(float(percentage_of_completion / 100))
+
+
+# System Settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
-# The frame of the app
+# Our App Frame
 app = customtkinter.CTk()
 app.geometry("720x480")
-app.title("Youtube downloader")
+app.title("Youtube Downloader")
 
-# Adding UI elements
+# Adding UI Elements
 title = customtkinter.CTkLabel(app, text="Insert a youtube link")
 title.pack(padx=10, pady=10)
 
-# Link input
+# Link Input
 url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
 
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+# Progress Percentage
+pPercentage = customtkinter.CTkLabel(app, text="0%")
+pPercentage.pack()
+
+# Progress Bar
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+
 # Download Button
 download = customtkinter.CTkButton(app, text="Download", command=startDownload)
-download.pack(padx=10,pady=10)
+download.pack(padx=10, pady=10)
 
-# Running the app as a loop
+# Run App
 app.mainloop()
